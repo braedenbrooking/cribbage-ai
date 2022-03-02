1ba1a655e646577eb88cbd1a3996a2c169ff7f84
braedenbrooking <57050183+braedenbrooking@users.noreply.github.com>
2022-03-02 00:14:29 -0500

    Game mechanics almost working just working on bugs in 15s calculation

--- crib.py
+++ crib.py
@@ -1,17 +1,22 @@
 import pydealer
 import random
 
+RANKS = {'values': {'Ace': 1, 'King': 13, 'Queen': 12, 'Jack': 11, '10': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2, 'Joker': 0}}
 
 class Player:
     __score = 0
     __myCrib = False
+    __name = ""
     hand = None
     handCopy = None
-    def __init__(self, myCrib):
-        self.__myCrib = myCrib
+    def __init__(self, name):
+        self.__name = name
+
+    def getName(self):
+        return self.__name
 
     def myCrib(self):
-        return __myCrib
+        return self.__myCrib
 
     def setmyCrib(self, isMyCrib):
         self.__myCrib = isMyCrib
@@ -20,9 +25,9 @@ class Player:
         return self.__score
 
     def scorePoints(self, points):
-        print("You scored: " + str(points) + " points")
+        print(self.getName() + " scored: " + str(points) + " points")
         self.__score += points
-        print("You now have a score of " + str(__score))
+        print(self.getName() + " now has a score of " + str(self.__score))
 
     def getHand(self):
         return self.hand
@@ -32,21 +37,30 @@ class Player:
 
     def deal6(self, deck):
         self.hand = deck.deal(6)
-        self.hand.sort()
+        self.hand.sort(RANKS)
+
+    def checkVictory(self):
+        if self.getScore() >= 120:
+            return True
+        else:
+            return False
 
     def discardPrompt(self, crib):
+        print(self.getName() + "'s hand:")
         print(self.hand)
         if self.myCrib():
-                print("It is your crib")
-            else:
-                print("It is not your crib")
+            print("It is your crib")
+        else:
+            print("It is not your crib")
         while self.hand.size > 4:
             card = input("Choose a card to discard: ")
-            try:
-                crib.add(self.hand.get(card))
+            startingSize = self.hand.size
+            crib.add(self.hand.get(card))
+            if startingSize > self.hand.size:
                 print("Successfully removed " + card)
-            except:
+            else:
                 print("Error")
+        self.copyHand()
         return crib
 
     def copyHand(self):
@@ -60,7 +74,7 @@ class Player:
         print("Cards in play: ")
         print(cardsOnTable)
         print("Sum to: " + str(sumOnTable))
-        print("Your hand: ")
+        print(self.getName() + "'s hand: ")
         print(self.handCopy)
 
         while True:
@@ -81,34 +95,40 @@ class Player:
                 self.handCopy.add(card)
 
             else:
+                sumOnTable += cardValue
                 cardsOnTable.add(card)
                 self.pegPoints(cardsOnTable, sumOnTable)
-                return sumOnTable+cardValue
+                return sumOnTable
     
     def pegPoints(self, cardsOnTable, sumOnTable):
         if cardsOnTable.size == 1:
             return
         
-        # Points for 15s and 31s
-        if sumOnTable == 15 or sumOnTable == 31:
+        # Points for 15s (31s counted elsewhere)
+        if sumOnTable == 15:
+            print("15 for 2")
             self.scorePoints(2)
 
         # Points for pairs, 3 of a kinds, and 4 of a kinds
-        if cardsOnTable[cardsOnTable.size-1] == cardsOnTable[cardsOnTable.size-2]:
-            if cardsOnTable.size > 2 and cardsOnTable[cardsOnTable.size-1] == cardsOnTable[cardsOnTable.size-3]:
-                if cardsOnTable.size > 3 and cardsOnTable[cardsOnTable.size-1] == cardsOnTable[cardsOnTable.size-4]:
+        if checkForPair(cardsOnTable[cardsOnTable.size-1], cardsOnTable[cardsOnTable.size-2]):
+            if cardsOnTable.size > 2 and checkForPair(cardsOnTable[cardsOnTable.size-1], cardsOnTable[cardsOnTable.size-3]):
+                if cardsOnTable.size > 3 and checkForPair(cardsOnTable[cardsOnTable.size-1], cardsOnTable[cardsOnTable.size-4]):
+                    print("4 of a kind for 12")
                     self.scorePoints(12)
                     return
                 else:
+                    print("3 of a kind for 6")
                     self.scorePoints(6)
                     return
             else:
+                print("Pair for 2")
                 self.scorePoints(2)
                 return
         
         # Points for runs (Not possible if there is a pair)
         top = pydealer.Stack()
         top.insert_list([cardsOnTable[cardsOnTable.size-1], cardsOnTable[cardsOnTable.size-2], cardsOnTable[cardsOnTable.size-3]],0)
+        top.sort(RANKS)
         runPoints = 0
         for i in range(4,cardsOnTable.size+1):
             if checkForRun(top):
@@ -117,23 +137,66 @@ class Player:
                 else:
                     runPoints += 1
                 top.add(cardsOnTable[cardsOnTable.size-i])
+                top.sort(RANKS)
             else:
                 break
         
         if runPoints > 0:
+            print(str(runPoints) + " for a run")
             self.scorePoints(runPoints)
 
-    def scoreHand(self, cutCard, scoringHand=self.hand):
+    def scoreHand(self, cutCard, scoringHand=None):
+        print()
+        if scoringHand is None:
+            scoringHand = self.hand
+        
+        print("Scoring hand: ")
+        print(scoringHand)
+        print("With Cut Card: ")
+        print(cutCard)
+        print()
+
         scoringHand.add(cutCard)
-        scoringHand.sort()
         totalHandScore = 0
+
+        # Flush
+        if scoringHand[0].suit == scoringHand[1].suit and scoringHand[0].suit == scoringHand[2].suit and scoringHand[0].suit == scoringHand[3].suit:
+            if scoringHand[0].suit == scoringHand[4].suit:
+                totalHandScore += 5
+                print("Flush including the cut for " + str(totalHandScore))
+            else:
+                totalHandScore += 4
+                print("Flush excluding the cut for " + str(totalHandScore))
+
+        
+        scoringHand.sort(RANKS) # For ease of calculations
+
         # 15s
+        handValues = [x.value for x in scoringHand]
+        find15s(handValues)
+
+
+
+        """
+        for i in range(scoringHand.size):
+            sum = convertCardToInt(scoringHand[i].value)
+            for j in range(i, scoringHand.size):
+                if sum + convertCardToInt(scoringHand[j].value) == 15:
+                    totalHandScore += 2
+                    print("Fifteen " + str(totalHandScore))
+                    break
+                elif sum + convertCardToInt(scoringHand[j].value) < 15:
+                    sum += convertCardToInt(scoringHand[j].value)
+
+
+
+
         for i in range(scoringHand.size): # Size should always be 5
             for j in range(i+1,scoringHand.size):
                 if convertCardToInt(scoringHand[i].value)+convertCardToInt(scoringHand[j].value) == 15:
                     totalHandScore += 2
-                    print("Fifteen " + str(totalHandScore) + "(" + scoringHand[i] + " + " + scoringHand[j] + ")")
-
+                    print("Fifteen " + str(totalHandScore) + "(" + str(scoringHand[i]) + " + " + str(scoringHand[j]) + ")")
+        """
 
 
         # 4 of a kinds, 3 of a kinds, and pairs
@@ -141,18 +204,18 @@ class Player:
         threeValue = None
         fourValue = None
         for i in range(scoringHand.size):
-            for j in range(scoringHand.size):
-                if scoringHand[i].value != scoringHand[j].value:
+            for j in range(i, scoringHand.size):
+                if not checkForPair(scoringHand[i], scoringHand[j]):
                     break
                 elif scoringHand[i].value == threeValue:
                     threeValue = None
                     fourValue = scoringHand[i].value
                     break
-                elif scoringHand[i].value in pairs:
-                    pairs.remove(scoringHand[i].value)
-                    threeValue = scoringHand.value
+                elif scoringHand[i].value in pairValues:
+                    pairValues.remove(scoringHand[i].value)
+                    threeValue = scoringHand[i].value
                 else:
-                    pairs.append(scoringHand[i].value)
+                    pairValues.append(scoringHand[i].value)
 
         for val in pairValues:
             totalHandScore += 2
@@ -160,50 +223,60 @@ class Player:
         
         if threeValue is not None:
             totalHandScore += 6
-            print("3 " + val + "s is " + str(totalHandScore))
+            print("3 " + threeValue + "s is " + str(totalHandScore))
         
         if fourValue is not None:
             totalHandScore += 12
-            print("4 " + val + "s is " + str(totalHandScore))
+            print("4 " + fourValue + "s is " + str(totalHandScore))
 
         
                 
 
         # Runs
-        stack = pydealer.Stack()
-        stack.insert_list([scoringHand[0], scoringHand[1], scoringHand[2]])
-        for i in range(4, scoringHand.size)
-            if checkForRun(stack):
-                stack.add(scoringHand[3])
-                if checkForRun(stack):
-                    totalHandScore += 4
-                    print("A run of 4 is " + str(totalHandScore))
+        runs = []
+        for i in range(scoringHand.size-2): # Runs of 3
+            for j in range(i, scoringHand.size-1):
+                for k in range(j, scoringHand.size):
+                    stack = pydealer.Stack()
+                    stack.insert_list([scoringHand[i], scoringHand[j], scoringHand[k]])
+                    if checkForRun(stack):
+                        runs.append([i,j,k])
                     
+        for run in runs: # Runs of more than 3
+            for l in range(scoringHand.size):
+                if l in run:
+                    continue
                 else:
-                    stack.get(str(scoringHand[3]))
-                    totalHandScore += 3
-                    print("A run of 3 is " + str(totalHandScore))
-        
-            stack.get(str(scoringHand[i-4]))
-            stack.add(scoringHand[i])
+                    stack = pydealer.Stack()
+                    stack.insert_list([scoringHand[run[0]], scoringHand[run[1]], scoringHand[run[2]], scoringHand[l]])
+                    if checkForRun(stack):
+                        run.append(l)
 
+        #Nobs (The right jack)
+        for card in scoringHand:
+            if card != cutCard and checkNobs(cutCard, card):
+                totalHandScore += 1
+                print("The right Jack (nobs) is" + str(totalHandScore))
 
+        self.scorePoints(totalHandScore) # Score points at the end
 
+def checkNobs(cut, card):
+    return card.value == 'Jack' and cut.suit == card.suit
 
-        
-            
-
-
-
+def checkForPair(card1, card2): # Helps to clean everything up
+    return card1.value == card2.value
 
 def checkForRun(stack): # checks if all the cards in the stack are sequent if not all returns False
-    stack.sort()
+    stack.sort(RANKS)
     for i in range(stack.size-1):
         if getNextValue(stack[i]) != stack[i+1].value:
             return False
     return True
 
-    
+def find15s(values):
+    print()
+    # No clue
+
 def getNextValue(card):
     if card.value == '10':
         return 'Jack'
@@ -226,17 +299,17 @@ def convertCardToInt(value):
         return int(value)
     except ValueError:
         return 10
-        
 
 def layCards(p1, p2):
-    currentTurnP1 = not p1.myCrib()
+    currentTurnP1 = p1.myCrib() # Needs to start reversed
     sumOnTable = 0
     cardsOnTable = pydealer.Stack()
     player1Go = False
     player2Go = False
     while p1.cardsRemaining() > 0 or p2.cardsRemaining() > 0 :
+        currentTurnP1 = not currentTurnP1
         newSumOnTable = 0
-        if currentTurnP1 not player1Go:
+        if currentTurnP1 and not player1Go:
             newSumOnTable = p1.promptToPlay(cardsOnTable, sumOnTable)
             if newSumOnTable == sumOnTable:
                 print("Player 1 says go")
@@ -248,9 +321,6 @@ def layCards(p1, p2):
                 return True
 
             if not p1.cardsRemaining() > 0:
-                if player2Go and newSumOnTable != 31:
-                    p1.scorePoints(1)
-                
                 player1Go = True
             
         elif not player2Go:
@@ -265,12 +335,11 @@ def layCards(p1, p2):
                 return True
 
             if not p2.cardsRemaining() > 0:
-                if player1Go and newSumOnTable != 31:
-                    p2.scorePoints(1)
                 player2Go = True
         
-        
-        if newSumOnTable == 31:
+        sumOnTable = newSumOnTable
+
+        if sumOnTable == 31:
             print("31!")
             if currentTurnP1:
                 p1.scorePoints(2)
@@ -286,23 +355,25 @@ def layCards(p1, p2):
             player1Go = False
             player2Go = False
 
+    print("Last card played")
+    if currentTurnP1:
+        p1.scorePoints(1)
+    else:
+        p2.scorePoints(1)
+
     return False #returns false if no one has claimed victory
-
-
-
 
 def cutTheDeck(deck):
     return deck.deal(1)[0]
 
-
-
 def main():
-    p1 = Player(False)
-    p2 = Player(True) 
+    p1 = Player("Player 1")
+    p2 = Player("Player 2")
     
     if random.randint(0,1): # Reverses the starting crib 50% of the time
         p1.setmyCrib(True)
-        p2.setmyCrib(False)
+    else:
+        p2.setmyCrib(True)
     
 
     while True:
@@ -310,20 +381,23 @@ def main():
         deck.shuffle()
         p1.deal6(deck)
         p2.deal6(deck)
-        
+        print("==CURRENT SCORE==")
+        print("Player 1: " + str(p1.getScore()))
+        print("Player 2: " + str(p2.getScore()))
         crib = pydealer.Stack()
         crib = p1.discardPrompt(crib)
         crib = p2.discardPrompt(crib)
-        crib.sort()
+        crib.sort(RANKS)
         cut = cutTheDeck(deck)
+        print("The cut card is: " + str(cut))
         if cut.value == 'Jack':
             if p1.myCrib():
                 p1.scorePoints(2)
             else:
                 p2.scorePoints(2)
 
-        if layCards(p1, p2):
-            break
+        if layCards(p1, p2): 
+            break # Victory Achieved
 
         if not p1.myCrib():
             p1.scoreHand(cut)
@@ -334,6 +408,4 @@ def main():
             p1.scoreHand(cut)
             p1.scoreHand(cut, crib)
 
-
-
-    
+main()
